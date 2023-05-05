7083ff4fcc415ca22782e2ee13a44a3f520c6178
손효재 <sonjw5128@gmail.com>
2023-05-05 20:10:42 +0900

    update : allow origins cofig

--- src/main/java/com/haeru/haeruworldback/global/config/WebConfig.java
+++ src/main/java/com/haeru/haeruworldback/global/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST")
+                .allowedOrigins("https://baru-haeru.vercel.app/")
+                .allowedMethods("GET")
                 .maxAge(3000);
     }
 }
